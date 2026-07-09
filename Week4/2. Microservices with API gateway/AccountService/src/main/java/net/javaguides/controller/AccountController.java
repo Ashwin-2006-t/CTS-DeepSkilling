861618df83e0d2384ce723f3d69327476a017fe6
Ashwin T <ashwin2006t@gmail.com>
2026-07-09 13:04:50 +0530

    week 4

---
+++ Week4/2. Microservices with API gateway/AccountService/src/main/java/net/javaguides/controller/AccountController.java
@@ -0,0 +1,16 @@
+package net.javaguides.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/account")
+    public String account() {
+
+        return "Account Service Running Successfully";
+
+    }
+
+}
